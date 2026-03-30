0a5ee38cb3e7a20de6d0ebbe3e2921d3153075ae
zHitrexx <drahomir.pop99@gmail.com>
2026-03-30 15:59:43 +0200

    Add files via upload

--- I2C/I2C.ino
+++ I2C/I2C.ino
@@ -1,9 +1,10 @@
 #define F_CPU 14745600UL
 #include <avr/io.h>
-// #include <avr/interrupt.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "src/usart.h"
 #include "src/BitOps.h"
 
@@ -15,11 +16,41 @@
 #define addr_w 0xD0
 #define addr_r 0xD1
 
+const char day_ch[7][3] = {{'P','o','\0'}, {'U','t','\0'}, {'S','t','\0'}, {'C','t','\0'}, {'P','a','\0'}, {'S','o','\0'}, {'N','e','\0'}};
+
+typedef struct 
+{
+  uint8_t hour;
+  uint8_t min;
+  uint8_t sec;
+  uint8_t day;
+  uint8_t date;
+  uint8_t month;
+  uint16_t year;
+} Time;
+
+Time time;
+
 void Setup(void)
 {
   DDR  = (1 << SCL) | (1 << SDA);
   PORT = (1 << SCL) | (1 << SDA);
   usart_setup(9600, bits8, parityNone, stop1);
+}
+
+void SetupTimer(void)
+{
+  TCCR1A = 0;
+  TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);
+  OCR1A = round(F_CPU / 1024 - 1);                       
+  TIMSK1 = (1 << OCIE1A); // ETIMSK
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  // Read values from RTC registers and wait
+  time = ReadTime();
+  printf("%02d:%02d:%02d | %s | %02d.%02d.%04d\r\n", time.hour, time.min, time.sec, day_ch[time.day - 1], time.date, time.month, time.year);
 }
 
 void Clk(void)
@@ -150,54 +181,158 @@ uint8_t ReadReg(uint8_t reg)
   return data;
 }
 
-void SetTime(uint8_t hour, uint8_t min, uint8_t sec, uint8_t day, uint8_t date, uint8_t month, uint16_t year)
+void SetTime(Time time)
 {
-  uint8_t month_bcd = ToBCD(month);
-  uint8_t year_short = year % 2000;
-  if (year / 100 == 21)
+  time = CheckValues(time);
+
+  uint8_t month_bcd = ToBCD(time.month);
+  uint8_t year_short = time.year % 2000;
+  if (time.year / 100 == 21) // Set century bit high if 21. century
   	Nastavit(month_bcd, 7);
   
-  WriteReg(0x00, ToBCD(sec)); // Seconds
-  WriteReg(0x01, ToBCD(min)); // Minutes
-  WriteReg(0x02, Vynulovat(ToBCD(hour), 6)); // Hours
-  WriteReg(0x03, ToBCD(day)); // Day
-  WriteReg(0x04, ToBCD(date)); // Date
-  WriteReg(0x05, month_bcd); // Month
+  WriteReg(0x00, ToBCD(time.sec)); // Seconds
+  WriteReg(0x01, ToBCD(time.min)); // Minutes
+  WriteReg(0x02, Vynulovat(ToBCD(time.hour), 6)); // Hours in 24h format (6. bit low)
+  WriteReg(0x03, ToBCD(time.day));  // Day
+  WriteReg(0x04, ToBCD(time.date)); // Date
+  WriteReg(0x05, month_bcd);   // Month
   WriteReg(0x06, ToBCD(year_short)); // Year
+
+  sei();
 }
 
-void ReadTime(void)
+Time ReadTime(void) // Vracet struct + nastavovat pomocí struct
 {
-  uint8_t sec         = ToDEC(ReadReg(0x00));
-  uint8_t min         = ToDEC(ReadReg(0x01));
-  uint8_t hour        = ToDEC(ReadReg(0x02));
-  uint8_t day         = ToDEC(ReadReg(0x03));
-  uint8_t date        = ToDEC(ReadReg(0x04));
+  Time time;
+  time.sec  = ToDEC(ReadReg(0x00));
+  time.min  = ToDEC(ReadReg(0x01));
+  time.hour = ToDEC(ReadReg(0x02));
+  time.day  = ToDEC(ReadReg(0x03));
+  time.date = ToDEC(ReadReg(0x04));
   uint8_t month_bcd   = ReadReg(0x05);
   uint8_t year_short  = ReadReg(0x06);
 
-  uint8_t month = ToDEC(month_bcd & 0x1F);
-  uint16_t year = 2000 + ToDEC(year_short);
+
+  time.month = ToDEC(month_bcd & 0x1F);
+  time.year = 2000 + ToDEC(year_short);
 
   if (JeNastaven(month_bcd, 7) == true)
-	year += 100;
+	time.year += 100;
 
-  printf("%02d:%02d:%02d %d %02d.%02d.%04d\r\n", hour, min, sec, day, date, month, year);  
+  return time; 
 }
 
+Time ProcessUSART(char *string) // Processing the command via USART
+{
+  Time time;
+  char *hour  = strtok(string, ":"); // Seperating the parameters 
+  char *min   = strtok(NULL, ":");
+  char *sec   = strtok(NULL, ",");
+  char *day   = strtok(NULL, ",");
+  char *date  = strtok(NULL, ".");
+  char *month = strtok(NULL, ".");
+  char *year  = strtok(NULL, "\0");
+
+  if (strcmp(hour, "HELP") == 0)
+  {
+    if (min == NULL)
+      printf("HELP:format | HELP:range | HELP:STOP\r\n");
+    else if (strcmp(min, "format") == 0)
+      printf("hours:minutes:seconds,day,date.month.year\r\n");
+    else if (strcmp(min, "range") == 0)
+      printf("[0-23]:[0-59]:[0-59],[1-7],[1-31].[1-12].[2000-2199]\r\n");
+    else if (strcmp(min, "STOP") == 0)
+      printf("Stops reading values from RTC\r\n");
+	else
+      printf("HELP:format | HELP:range\r\n");
+    return;
+  }
+
+  if (strcmp(hour, "STOP") == 0)
+  {
+    cli();
+    return;
+  }
+
+  if (hour == NULL || min == NULL || sec == NULL || day == NULL || date == NULL || month == NULL || year == NULL)
+  {
+    printf("Missing parameter! Try HELP :)\r\n");
+    return;
+  }
+	
+  time.sec   = atoi(sec);  // Conversion from string to number
+  time.min   = atoi(min);
+  time.hour  = atoi(hour);
+  time.day   = atoi(day);
+  time.date  = atoi(date);
+  time.month = atoi(month);
+  time.year  = atoi(year);
+
+  
+  return CheckValues(time);
+}
+
+Time CheckValues(Time time)
+{
+  if (time.hour > 23)
+    time.hour = 0;
+  if (time.min > 59)
+    time.min = 0;
+  if (time.sec > 59)
+    time.sec = 0;
+  if (time.day > 7 || time.day == 0)
+    time.day = 1;
+  if (time.date > 31 || time.date == 0)
+    time.date = 1;
+  if (time.month > 12 || time.month == 0)
+    time.month = 1;
+  if (time.year > 2199 || time.year < 2000)
+    time.year = 2000;
+
+  if (time.date > 29 && time.month == 2)
+    time.date = 1;
+  if (time.date > 30 && ((time.month < 8 && time.month % 2 == 0) || (time.month > 7 && time.month % 2 != 0)))
+    time.date = 1;
+
+  return time;
+}
 
 int main(void)
 {
-  Setup();
+  Time time;
+  char znak;
+  char string[23];
+  uint8_t index = 0;
 
-  // Set values in RTC registers
-  SetTime(8,0,0,2,17,3,2026);
+  Setup();
+  SetupTimer();
+
+  printf("Zadejte cas (HELP)!\r\n");
 
   while(1)
   {
-  // Read values from RTC registers and wait
-  ReadTime();
-  _delay_ms(1000);
+  if (usart_dataready())
+	  {
+	    znak = usart_getchar();
+      printf("%c", znak); // Vymazat a nastavit local line editing a local echo v terminal v putty
+	    if (znak == '\r' || znak == '\n')
+	    {
+	      if (index == 0)
+	        continue;
+          string[index] = '\0';
+          time = ProcessUSART(string); // Set values in RTC registers
+          SetTime(time);
+	      index = 0;
+	    }
+	    else
+	    {
+	      if (index < 23)
+	      {
+	        string[index] = znak;
+	        index++;
+	      }
+	    }
+	  }
   }
   return 0;
 }
