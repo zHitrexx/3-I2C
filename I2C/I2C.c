5c6b2b7a778772237527b0854d8352b035ff2140
zHitrex <drahomir.pop99@gmail.com>
2026-06-26 17:34:27 +0200

    Update I2C.c

--- I2C/I2C.c
+++ I2C/I2C.c
@@ -1,3 +1,6 @@
+//=====================================//
+// Made by Hitrex (github.com/zHitrex) //
+//=====================================//
 #define F_CPU 14745600UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
